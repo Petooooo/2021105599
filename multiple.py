3de010d534d1dadd6a6460a5c61de4eacc33e130
박지환 <peto@khu.ac.kr>
2022-04-08 11:28:13 +0900

    code modifed

--- multiple.py
+++ multiple.py
@@ -1,3 +1,4 @@
 for i in range(1, 9):
-    for j in range(1, 9):
-        print("{} * {} = {}".format(i, j, i*j))
+    for j in range(1, 10):
+        print("{} * {} = {}".format(j, i, i*j), end="\t")
+    print()
